564b1c92f8028f0648af9f4f52b2317a42b2100f
Harim Choi <harim1206@gmail.com>
2018-02-08 16:50:10 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,3 +6,15 @@ end
 def array_with_two_elements
   array = [1,2]
 end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[arr.count-1]
+end
